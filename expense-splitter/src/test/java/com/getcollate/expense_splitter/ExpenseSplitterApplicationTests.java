0afc464221af5c23472b13d1af16235864905aa0
developer-psd <developer.pranavsrinivasdutta@gmail.com>
2026-03-12 17:44:53 +0530

    Initial project setup with the standalone as the architectural boundrary for the spring framework.

---
+++ expense-splitter/src/test/java/com/getcollate/expense_splitter/ExpenseSplitterApplicationTests.java
@@ -0,0 +1,15 @@
+package com.getcollate.expense_splitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseSplitterApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("Inside Spring Tests... Sairam...");
+		assert true;
+	}
+
+}
